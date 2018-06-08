b889255f8e550fc59a3569920242223377d44a4c
CircleLin <nilcric@outlook.com>
2018-06-08 10:06:44 +0800

    damage calc and critical calc

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
-var damate = new Vue({
-  el: '#damage',
+function range(val, min, max) {
+  if (val < min) return min;
+  if (val > max) return max;
+  return val;
+}
+
+function elementrange(element, val) {
+  if (element==0.5) return range(val, 0.1, 0.9);
+  if (element==1.0) return range(val, 0.6, 1.4);
+  if (element==2.0) return range(val, 1.6, 2.4);
+}
+
+var damagecalc = new Vue({
+  el: '#damagecalc',
   data: {
     ATK: null,
     DEF: null,
@@ -11,19 +23,35 @@ var damate = new Vue({
     DEFElement: null,
     element: 1.0,
     critical: false,
-    jump: 1
+    jump: 1.0
   },
   computed: {
     damage: function() {
       return parseInt(1.0
         * this.ATK / this.DEF * this.skill / 6 
-        * (this.ATKbuff?1+this.ATKbuff/100:1) 
+        * range(this.ATKbuff?1+this.ATKbuff/100:1, 0.5, 2.5)
+        / range(this.DEFbuff?1+this.DEFbuff/100:1, 0.33, 2.0)
+        * elementrange(this.element, (this.element + (this.ATKElement?this.ATKElement:0)/100 - (this.DEFElement?this.DEFElement:0)/100))
         * (this.oncebuff?1+this.oncebuff/100:1) 
         * (this.critical?1.5:1.0) 
         * (this.jump?this.jump:1) 
-        * (Number(this.element) + (this.ATKElement?this.ATKElement:0)/100 - (this.DEFElement?this.DEFElement:0)/100) 
-        / (this.DEFbuff?1+this.DEFbuff/100:1)
       )
+    }
+  }
+});
+
+var criticalcalc = new Vue({
+  el: '#criticalcalc',
+  data: {
+    luck0: null,
+    luck1: null,
+    buff0: null,
+    buff1: null,
+    element: 1.0
+  },
+  computed: {
+    critical: function() {
+      return parseInt(this.element==0.5?0:(this.luck0*1.2*(this.element==2.0?1.1:1.0)*(1+this.buff0/100)-this.luck1*(1+this.buff1/100)))
     }
   }
 });

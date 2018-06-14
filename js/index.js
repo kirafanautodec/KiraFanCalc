6f89e00a4f9d492742492958d5905c66dd970913
CircleLin <nilcric@outlook.com>
2018-06-14 15:07:51 +0800

    trans

--- js/index.js
+++ js/index.js
@@ -1,30 +1,56 @@
 var trans = {
   lang: {j:0, e:1, z:2},
-  app: ["きらファン計算機", "KiraFanCalc", "KiraFan 计算器"],
+  'きらファン計算機': ["きらファン計算機", "KiraFanCalc", "KiraFan 计算器"],
 
-  home: ["ホーム", "Home", "主页"],
-  damagecalc: ["ダメージ", "Damage", "伤害"],
-  criticalcalc: ["クリ率", "Critical", "暴击"],
-  ordercalc: ["タイムライン", "Timeline", "行动条"],
-  statuscalc: ["ステータス", "Status", "角色数值"],
+  'ホーム': ["ホーム", "Home", "主页"],
+  'ダメージ': ["ダメージ", "Damage", "伤害"],
+  'クリ率': ["クリ率", "Critical", "暴击"],
+  'タイムライン': ["タイムライン", "Timeline", "行动条"],
+  'ステータス': ["ステータス", "Status", "角色数值"],
 
-  damagecalculation: ["ダメージ計算", "Damage Calculation", "伤害计算"],
+  'ダメージ計算': ["ダメージ計算", "Damage Calculation", "伤害计算"],
 
-  damageatk: ["味方攻撃", "ATK", "我方攻击"],
-  damagedef: ["敵防御", "DEF", "敌方防御"],
-  damageskill: ["スキル", "Skill", "技能"],
-  damageoncebuff: ["一度バフ", "Next-turn buff", "一度buff"],
-  damageatkbuff: ["味方攻撃バフ", "ATK buff", "我方攻击buff"],
-  damagedefbuff: ["敵防御バフ", "DEF buff", "敌方防御buff"],
-  damageatkelement: ["味方属性バフ", "ATK element buff", "我方属性buff"],
-  damagedefelement: ["敵属性バフ", "DEF element buff", "敌方属性buff"],
-  damageresult: ["ダメージ＝", "Damage =", "伤害 ="],
+  '味方攻撃': ["味方攻撃", "ATK", "我方攻击"],
+  '敵防御': ["敵防御", "DEF", "敌方防御"],
+  'スキル': ["スキル", "Skill", "技能"],
+  '一度バフ': ["一度バフ", "Next-turn buff", "一度buff"],
+  '味方攻撃バフ': ["味方攻撃バフ", "ATK buff", "我方攻击buff"],
+  '敵防御バフ': ["敵防御バフ", "DEF buff", "敌方防御buff"],
+  '味方属性バフ': ["味方属性バフ", "ATK element buff", "我方属性buff"],
+  '敵属性バフ': ["敵属性バフ", "DEF element buff", "敌方属性buff"],
+  'ダメージ＝': ["ダメージ＝", "Damage =", "伤害 ="],
 
-  optioncritical: ["クリティカル", "Critical", "暴击"],
-  optionelement2: ["有利", "Effective", "有利"],
-  optionelement1: ["普通", "Normal", "普通"],
-  optionelement0: ["不利", "Uneffective", "不利"],
+  'クリティカル': ["クリティカル", "Critical", "暴击"],
+  '有利': ["有利", "Effective", "有利"],
+  '普通': ["普通", "Normal", "普通"],
+  '不利': ["不利", "Uneffective", "不利"],
 
+  'クリティカル率計算': ['クリティカル率計算', 'Critical Probability Calculation', '暴击率计算'],
+
+  '味方幸運': ['味方幸運', 'LUK', '我方幸运'],
+  '敵幸運': ['敵幸運', 'Enemy\'s LUK', '敌方幸运'],
+  '味方幸運バフ': ['味方幸運バフ', 'LUK buff', '我方幸运buff'],
+  '敵幸運バフ': ['敵幸運バフ', 'Enemy\'s LUK buff', '敌方幸运buff'],
+
+  'クリティカル率＝': ['クリティカル率＝', 'Critical Probability =', '暴击率 ='],
+
+  '硬直値計算': ['硬直値計算', 'Order Value Calculation', '行动值计算'],
+  'スキル硬直': ['スキル硬直', 'Skill load factor', '技能行动系数'],
+  'SPDバフ': ['SPDバフ', 'SPD buff', 'SPD buff'],
+
+  '硬直値＝': ['硬直値＝', 'Order Value =', '行动值 ='],
+
+  'タイムライン模擬': ['タイムライン模擬', 'Timeline Simulation', '行动条模拟'],
+
+  'ステータス計算': ['ステータス計算', 'Status Calculation', '角色数值计算'],
+
+  'せんし': ['せんし', 'Fighter', '战士'],
+  'まほうつかい': ['まほうつかい', 'Magician', '魔法使'],
+  'そうりょ': ['そうりょ', 'Priest', '僧侣'],
+  'ナイト': ['ナイト', 'Knight', '骑士'],
+  'アルケミスト': ['アルケミスト', 'Alchemist', '炼金术士'],
+
+  '開発中': ['開発中', 'Coming soon', '咕咕咕'],
 };
 
 function range(val, min, max) {
@@ -53,7 +79,13 @@ Vue.component('trans', {
           break;
         }
       }
-      return trans[t][lang]//this.text[t][lang]
+      try {
+        return trans[t][lang]//this.text[t][lang]
+      }
+      catch (e)
+      {
+        return t
+      }
     }
   },
   template: '<span>{{trans(t)}}</span>'
